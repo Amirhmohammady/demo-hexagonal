509e337bafb4d0298f4aa68970f7a46939c6ed10
amir_h_mohammady <amir_h_mohammady@hotmail.com>
2023-09-26 23:56:39 +0330

    GetAppointmentException.class added to ExceptionHandlerController

--- infrastructure/src/main/java/org/example/appointment/web/exceptionhandler/ExceptionHandlerController.java
+++ infrastructure/src/main/java/org/example/appointment/web/exceptionhandler/ExceptionHandlerController.java
@@ -1,7 +1,20 @@
 package org.example.appointment.web.exceptionhandler;
 
+import org.example.appointment.domain.appointment.service.exceptions.GetAppointmentException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class ExceptionHandlerController {
+public class ExceptionHandlerController extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(value = {GetAppointmentException.class})
+    public ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
+        String bodyOfResponse = ex.getMessage();
+        return handleExceptionInternal(ex, bodyOfResponse,
+                new HttpHeaders(), HttpStatus.CONFLICT, request);
+    }
 }
