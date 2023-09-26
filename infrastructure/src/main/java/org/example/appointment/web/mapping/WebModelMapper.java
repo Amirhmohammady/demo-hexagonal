7965b415ccf14c827376c90a2a89f9f3147a81e4
amir_h_mohammady <amir_h_mohammady@hotmail.com>
2023-09-26 23:39:46 +0330

    appointment controller return value fixed

--- infrastructure/src/main/java/org/example/appointment/web/mapping/WebModelMapper.java
+++ infrastructure/src/main/java/org/example/appointment/web/mapping/WebModelMapper.java
@@ -1,9 +1,14 @@
 package org.example.appointment.web.mapping;
 
+import org.example.appointment.domain.appointment.model.Appointment;
+import org.example.appointment.web.model.AppointmentRequset;
 import org.mapstruct.Mapper;
 
 @Mapper
 public interface WebModelMapper {
     org.example.appointment.domain.doctor.model.Doctor map(org.example.appointment.web.model.Doctor doctor);
+
     org.example.appointment.web.model.Doctor map(org.example.appointment.domain.doctor.model.Doctor doctor);
+
+    AppointmentRequset map(Appointment appointment);
 }
--- infrastructure/src/main/java/org/example/appointment/web/resources/AppointmentResources.java
+++ infrastructure/src/main/java/org/example/appointment/web/resources/AppointmentResources.java
@@ -2,6 +2,7 @@ package org.example.appointment.web.resources;
 
 import lombok.RequiredArgsConstructor;
 import org.example.appointment.domain.appointment.port.api.AppointmentService;
+import org.example.appointment.web.mapping.WebModelMapper;
 import org.example.appointment.web.model.Appointment;
 import org.example.appointment.web.model.AppointmentRequset;
 import org.example.appointment.web.securityconfig.AuthenticationFacade;
@@ -19,20 +20,16 @@ import java.util.List;
 public class AppointmentResources {
     private final AppointmentService appointmentService;
     private final AuthenticationFacade authenticationFacade;
+    private final WebModelMapper mapper;
 
     @GetMapping("/appointments")
-    public List<Appointment> findAppointments(
-            @RequestParam(value = "date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate appointmentDate
-    ) {
+    public List<Appointment> findAppointments(@RequestParam(value = "date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate appointmentDate) {
         return Collections.emptyList();
     }
 
     @Secured({"ROLE_PATIENT"})
     @PostMapping("/appointments")
-    public AppointmentRequset setAppointments(
-            @RequestBody AppointmentRequset appointment
-    ) {
-        appointmentService.createNewAppointment(appointment.doctorId(),authenticationFacade.getCurrentUser().getId(),appointment.startTime(),appointment.duration());
-        return null;
+    public AppointmentRequset setAppointments(@RequestBody AppointmentRequset appointment) {
+        return mapper.map(appointmentService.createNewAppointment(appointment.doctorId(), authenticationFacade.getCurrentUser().getId(), appointment.startTime(), appointment.duration()));
     }
 }
